b8922978bb9bdf6f4f044a35feab553e1278ee2d
Can Vardar <ismailcanvardar@gmail.com>
2024-08-11 04:47:54 +0300

    chore: update voc.config.ts

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
+  ogImageUrl: 'https://avatars.githubusercontent.com/u/26247827?s=400&u=b7fdbd5851143112c64655a7c2a2cb54c4af2831&v=4',
   baseUrl: "https://icanvardar.com",
   title: 'Can Vardar',
   titleTemplate: 'Can Vardar - %s',
