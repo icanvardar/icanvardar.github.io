4272f8c89909ff9b57d446ee7e1730ec8ae2c868
icanvardar <icanvardar@users.noreply.github.com>
2024-11-15 18:51:36 +0000

    deploy: 6ea2ac94592271c5ed34dae65623b37b5f96782f

--- assets/index-E_mIcqA9.js
+++ assets/index-E_mIcqA9.js
@@ -1,4 +1,4 @@
-import{u as t,j as e}from"./index-Jy2miSgv.js";const o={layout:"landing"};function r(i){const n={a:"a",code:"code",div:"div",h2:"h2",li:"li",p:"p",ul:"ul",...t(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx("img",{style:{borderRadius:24},width:"156px",src:"/can-vardar.jpeg"}),`
+import{u as t,j as e}from"./index-DtmtN6bS.js";const o={layout:"landing"};function r(i){const n={a:"a",code:"code",div:"div",h2:"h2",li:"li",p:"p",ul:"ul",...t(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx("img",{style:{borderRadius:24},width:"156px",src:"/can-vardar.jpeg"}),`
 `,e.jsx(n.p,{children:"Hello! I'm Can."}),`
 `,e.jsx(n.p,{children:"I’m a software engineer with a passion for learning and continuous improvement. Over the years, I've had the privilege of working with various companies, where I always strive to deliver my best work."}),`
 `,e.jsx(n.p,{children:"My journey in software development has made me a programming language polyglot, capable of adapting to different technologies and environments. Learning is a personal philosophy of mine, and I’m committed to growing in my craft every day."}),`
@@ -7,6 +7,7 @@ import{u as t,j as e}from"./index-Jy2miSgv.js";const o={layout:"landing"};functi
 `,e.jsxs(n.h2,{id:"projects",children:["Projects",e.jsx(n.a,{"aria-hidden":"true",tabIndex:"-1",href:"#projects",children:e.jsx(n.div,{"data-autolink-icon":!0})})]}),`
 `,e.jsx(n.p,{children:"I enjoy creating libraries/tools, and here are some of my recent side projects:"}),`
 `,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/icanvardar/arbor",children:e.jsx(n.code,{children:"arbor"})}),": A trie-based autocomplete suggestion tool written in Rust."]}),`
 `,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/icanvardar/cubipods",children:e.jsx(n.code,{children:"cubipods"})}),": A minimal EVM written in Rust."]}),`
 `,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/icanvardar/gasgnome",children:e.jsx(n.code,{children:"gasgnome"})}),": Collection of Solidity libraries written in inline assembly."]}),`
 `,e.jsxs(n.li,{children:[e.jsx(n.a,{href:"https://github.com/icanvardar/plaxis",children:e.jsx(n.code,{children:"plaxis"})}),": Type-safe array management library for JavaScript and TypeScript."]}),`
